4f0f996ab5e4b842c6d4aca826ca8d439f6cc600
white <white@DESKTOP-D1M2GIS>
2024-01-26 00:47:35 +0100

    Add project files.

---
+++ miniKIFIR/StudentModifyWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace miniKIFIR
+{
+    public partial class StudentModifyWindow : Window
+    {
+        private Students editedStudent;
+        public StudentModifyWindow(Students student)
+        {
+            InitializeComponent();
+            editedStudent = student;
+            txtOmAzonosito.Text = editedStudent.OM_Azonosito;
+        }
+
+        public bool ChangesSaved { get; private set; } = false;
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            editedStudent.OM_Azonosito = txtOmAzonosito.Text;
+            ChangesSaved = true;
+            Close();
+        }
+        private void windowCloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                e.Handled = true;
+                TraversalRequest request = new TraversalRequest(FocusNavigationDirection.Next);
+                UIElement elementWithFocus = Keyboard.FocusedElement as UIElement;
+
+                if (elementWithFocus != null)
+                {
+                    elementWithFocus.MoveFocus(request);
+                }
+            }
+        }
+    }
+}
+
---
+++ miniKIFIR/Students.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniKIFIR
+{
+    public class Students : IFelvetelizo
+    {
+        string oM_Azonosito;
+        string neve;
+        string email; 
+        DateTime szuletesiDatum;
+        string ertesitesiCime;
+        int matematika;
+        int magyar;
+
+        public Students(string oM_Azonosito, string neve, string email, DateTime szuletesiDatum, string ertesitesiCime, int matematika, int magyar)
+        {
+            this.oM_Azonosito = oM_Azonosito;
+            this.neve = neve;
+            this.email = email;
+            this.szuletesiDatum = szuletesiDatum;
+            this.ertesitesiCime = ertesitesiCime;
+            this.matematika = matematika;
+            this.magyar = magyar;
+        }
+
+        public string OM_Azonosito { get => oM_Azonosito; set => oM_Azonosito = value; }
+        public string Neve { get => neve; set => neve = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime SzuletesiDatum { get => szuletesiDatum; set => szuletesiDatum = value; }
+        public string ErtesitesiCime { get => ertesitesiCime; set => ertesitesiCime = value; }
+        public int Matematika { get => matematika; set => matematika = value; }
+        public int Magyar { get => magyar; set => magyar = value; }
+
+        public string CSVSortAdVissza()
+        {
+            string line = $"{oM_Azonosito};{neve};{email};{szuletesiDatum.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)};{ertesitesiCime};{matematika};{magyar}";
+            return line;
+        }
+
+        public void ModositCSVSorral(string csvString)
+        {
+            string[] studentArray = csvString.Split(',');
+            if (studentArray.Length == 7)
+            {
+                oM_Azonosito = studentArray[0];
+                neve = studentArray[1];
+                email = studentArray[2];
+                szuletesiDatum = DateTime.ParseExact(studentArray[3], "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                ertesitesiCime = studentArray[4];
+                matematika = int.Parse(studentArray[5]);
+                magyar = int.Parse(studentArray[6]);
+            }
+        }
+    }
+}
